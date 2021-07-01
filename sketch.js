887870642c1e3e3bb97e3f3d71b5ba1a4bb6798b
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2021-07-01 17:48:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var state="login"
+var database 
+var x
+var check
+var y
+
+function preload(){
+
+}
+
+function setup(){
+document.getElementById("mc").style.display="none"
+
+}
+function draw(){
+    x= document.getElementById("name").value;
+    y=document.getElementById("pass").value;
+
+
+    
+}
+function check(){
+    if(perm==="block"){
+        alert(" This account is locked!!")
+    }
+    if(x==="Pushkar"&&y==="Pushkar@9899"||x==="Himanshu"&&y==="Himanshu@9807"){
+        var perm="allow"
+        if(perm==="allow"){
+            document.getElementById("form").style.display="none"
+            document.getElementById("mc").style.display="block"
+        }
+        else{
+            alert("This id is either banned or doesn't exist")
+        }
+    }
+    else{
+     alert("Wrong Id And Password");
+    }
+}
