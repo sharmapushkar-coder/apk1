9ed2d898216fc0327ede65789f374925eb4825db
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2021-07-03 09:04:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var n
+var img
+function preload(){
+    img=loadImage("kelly.jpg")
+}
+function setup(){
+    var s=new p5.Speech()
+    s.speak("Welcome to Pushkar's guild progress tracking app.")
+}
+function draw(){
+  //n=document.getElementById("name").value;  
+}
+function speak(){
+    
+}
